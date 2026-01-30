872b35368be62249e03a610adef2ee0a8e1b26c0
padmadarjicomp32 <padmacomp32@gmail.com>
2026-01-30 13:51:13 +0530

    Create template folder in project folder, add static assests to restaurant app

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,4 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'restaurant/static/'
+
+STATICFILES_DIRS =[
+    "restaurant/static"
+]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def sayHello(request):
+    return HttpResponse("Welcome To Little Lemon")
+
+def home(request):
+    return render(request,'index.html',{})
+
+def about(request):
+    return render(request,"index.html",{})
+
+def book(request):
+    return render(request,"index.html",{})
+
+def menu(request):
+    return render(request,"index.html",{})
+
+def display_menu_item(request):
+    return render(request,"index.html",{})
